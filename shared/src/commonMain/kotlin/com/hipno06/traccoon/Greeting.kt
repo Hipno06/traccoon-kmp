649a4f4b2a8d81aabe4062a9ef106cca292b06b6
Hipno06 <eylayvictor@gmail.com>
2026-05-25 08:48:31 +0100

    chore: delete default Greeting template

--- shared/src/commonMain/kotlin/com/hipno06/traccoon/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.hipno06.traccoon
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return sayHello(platform.name)
-    }
-}
--- shared/src/commonMain/kotlin/com/hipno06/traccoon/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.hipno06.traccoon
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
